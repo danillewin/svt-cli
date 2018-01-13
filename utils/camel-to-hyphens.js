4dfac1d904b6d00ae2948a5379d2efb1548fdb90
Danil Lewin <danillewin@gmail.com>
2018-01-13 16:52:43 +0300

    baad

--- utils/camel-to-hyphens.js
+++ utils/camel-to-hyphens.js
@@ -1,5 +1,5 @@
 const camelToHyphens = function(name) {
-    return name.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+    return name.replace(/([a-z])([A-Z])/g, '$2-$1').toLowerCase();
 };
 
 module.exports = camelToHyphens;
